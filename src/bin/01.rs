6cbfdacd9a32c0fce7530e9bb262869ac3af4c7d
jbiccler <jaron.biccler@gmail.com>
2025-12-01 15:14:31 +0100

    Refactor: day01 replace enum and improve parsing perf

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,25 +1,22 @@
 advent_of_code::solution!(1);
 
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    Left(i32),
-    Right(i32),
-}
+const BOUND: i32 = 100;
+const START: i32 = 50;
 
-fn parse_input(input: &str) -> Vec<Direction> {
+fn parse_input(input: &str) -> Vec<i32> {
     input
         .lines()
-        .filter(|l| !l.is_empty())
-        .map(|l| {
-            let line = l.trim();
-            let dir = &line[0..1];
-            let val = &line[1..]
-                .parse::<i32>()
-                .expect("Failed to parse digit in input");
+        .filter_map(|l| {
+            let l = l.trim();
+            if l.is_empty() {
+                return None;
+            };
+            let (dir, val) = l.split_at(1);
+            let val = val.parse::<i32>().ok()?;
             match dir {
-                "L" => Direction::Left(*val),
-                "R" => Direction::Right(*val),
-                _ => panic!("Wrong input for direction"),
+                "L" => Some(-val),
+                "R" => Some(val),
+                _ => None,
             }
         })
         .collect()
@@ -27,60 +24,46 @@ fn parse_input(input: &str) -> Vec<Direction> {
 
 /// Calculate next value wrapping around bound 0..=99;
 #[inline(always)]
-fn wrap(current: i32, dir: &Direction) -> i32 {
-    match dir {
-        Direction::Left(v) => (current - v).rem_euclid(100),
-        Direction::Right(v) => (current + v).rem_euclid(100),
-    }
+fn wrap(current: i32, dir: i32) -> i32 {
+    (current + dir).rem_euclid(BOUND)
 }
 
-fn solve_part1(ins: &[Direction]) -> u64 {
-    let mut count = 0;
-    // Start value as per instructions is 50
-    let mut prev = 50;
-    for i in ins.iter() {
-        prev = wrap(prev, i);
-        if prev == 0 {
-            count += 1;
-        }
-    }
-    count
+fn solve_part1(ins: &[i32]) -> u64 {
+    ins.iter()
+        .scan(START, |prev, dir| {
+            *prev = wrap(*prev, *dir);
+            Some(*prev == 0)
+        })
+        .filter(|&passed_zero| passed_zero)
+        .count() as u64
 }
 
 /// Calculate next value wrapping around bound 0..=99 and return number of times we passed 0 in the
 /// process.
 #[inline(always)]
-fn wrap_count_passing_zero(current: i32, dir: &Direction) -> (i32, i32) {
-    let next = match dir {
-        Direction::Left(v) => (current - v).rem_euclid(100),
-        Direction::Right(v) => (current + v).rem_euclid(100),
-    };
-    let passed_zero = match dir {
-        Direction::Left(v) => {
-            // Correction needed for first passage through zero as this is not captured by whole
-            // division. If current was already == 0, then this was already counted in the previous
-            // cycle.
-            if (current != 0) && (current - v) <= 0 {
-                ((current - v) / 100).abs() + 1
-            } else {
-                ((current - v) / 100).abs()
-            }
-        }
-        Direction::Right(v) => (current + v) / 100,
-    };
-    (next, passed_zero)
+fn wrap_count_passing_zero(current: i32, dir: i32) -> (i32, i32) {
+    let next = (current + dir).rem_euclid(BOUND);
+    if dir >= 0 {
+        // Right case
+        (next, (current + dir) / BOUND)
+    } else {
+        // Left case
+        // Correction needed for first passage through zero as this is not captured by whole
+        // division. If current was already == 0, then this was already counted in the previous
+        // cycle.
+        let correction = ((current != 0) && (current + dir) <= 0) as i32;
+        (next, ((current + dir) / BOUND).abs() + correction)
+    }
 }
 
-fn solve_part2(ins: &[Direction]) -> u64 {
-    let mut count = 0;
-    // Start value as per instructions is 50
-    let mut prev = 50;
-    for i in ins.iter() {
-        let (prev_tmp, passing) = wrap_count_passing_zero(prev, i);
-        prev = prev_tmp;
-        count += passing as u64;
-    }
-    count
+fn solve_part2(ins: &[i32]) -> u64 {
+    ins.iter()
+        .scan(START, |prev, dir| {
+            let (prev_tmp, passing) = wrap_count_passing_zero(*prev, *dir);
+            *prev = prev_tmp;
+            Some(passing)
+        })
+        .sum::<i32>() as u64
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
